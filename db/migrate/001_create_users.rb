4d26ea924c32f5d689c4ab9d6ea94a6d4c8724b0
VictoriaMeng <1ststarofthenight@gmail.com>
2018-08-01 12:35:10 -0700

    wrote migrations

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
-  create_table :users do |t|
-    t.string :name
-    t.string :password_digest
+  
+  def change 
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+    end
   end
+
 end
