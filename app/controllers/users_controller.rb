be32c876e788a65f29b4e1d9318bc479a14aa44d
VictoriaMeng <1ststarofthenight@gmail.com>
2018-08-01 15:04:25 -0700

    completed new and create user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,24 @@ class UsersController < ApplicationController
   end
 
   def create 
+    binding.pry
+    @user = User.new(user_params)
+    binding.pry
+    if @user.valid?
+      @user.save 
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end 
+  end
+
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
