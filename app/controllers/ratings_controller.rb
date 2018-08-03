e094be47eef39b380906a0f364b74ac40126f6b8
VictoriaMeng <1ststarofthenight@gmail.com>
2018-08-03 11:42:17 -0700

    wrote ratings#create action with validations and redirects

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,6 +4,22 @@ class RatingsController < ApplicationController
   end 
 
   def create 
-    binding.pry
+    @franchise = Franchise.find(params[:franchise_id])
+    @user = User.find(session[:user_id])
+    @rating = Rating.new(ratings_params)
+    @rating.franchise = @franchise 
+    @rating.user = @user
+    if @rating.valid?
+      @rating.save 
+      redirect_to franchise_path(@franchise)
+    else 
+      redirect_to new_franchise_rating_path(@franchise)
+    end
+  end
+
+  private 
+
+  def ratings_params 
+    params.permit(:stars)
   end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,5 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :franchise
 
+
 end
