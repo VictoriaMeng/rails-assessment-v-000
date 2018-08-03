cc5393ddb231b7ffa5af1e4911df56143136b2ab
VictoriaMeng <1ststarofthenight@gmail.com>
2018-08-02 17:12:12 -0700

    Added index and show Franchise views with actions and routes

--- app/controllers/franchises_controller.rb
+++ app/controllers/franchises_controller.rb
@@ -1,2 +1,9 @@
 class FranchisesController < ApplicationController 
+  def index 
+    @franchises = Franchise.all
+  end
+
+  def show 
+    @franchise = Franchise.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :users
+  resources :franchises
 end
