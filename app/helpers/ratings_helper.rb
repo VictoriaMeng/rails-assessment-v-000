5fe16d159546aee6d22fac11a0a56890692c644c
VictoriaMeng <1ststarofthenight@gmail.com>
2018-08-07 20:01:52 -0700

    corrected display_listed_rating path, now links to franchise page instead of non-existing ratings show page

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,5 +1,5 @@
 module RatingsHelper 
   def display_listed_rating(rating)
-    link_to("#{rating.franchise.name} - #{rating.stars} stars", franchise_rating_path(rating) )
+    link_to("#{rating.franchise.name} - #{rating.stars} stars", franchise_path(rating.franchise) )
   end
 end
